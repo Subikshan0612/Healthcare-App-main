81fc831288df32883df77ffa642bce060c412426
Subikshan0612 <subishan1234@gmail.com>
2024-12-30 10:20:31 +0530

    Email Confirmation

--- backend/server.js
+++ backend/server.js
@@ -42,4 +42,4 @@ app.use((err, req, res, next) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`✨Server running on http://localhost:${PORT}`));
